5c19710a95338e95d28403a83783114fcfb34fc3
toubal77 <72252709+toubal77@users.noreply.github.com>
2021-08-03 18:41:21 +0200

    add fonction geolocator and update configuration

--- lib/widgets/map_app.dart
+++ lib/widgets/map_app.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapApp extends StatefulWidget {
@@ -13,12 +14,27 @@ class _MapAppState extends State<MapApp> {
 
   // ignore: unused_field
   late GoogleMapController _googleController;
+  double bottomPaddingMap = 0;
+  void locatePosition() async {
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    Position currentPosition = position;
+    LatLng latLatPosistion = LatLng(position.latitude, position.longitude);
+    CameraPosition cameraPosition =
+        new CameraPosition(target: latLatPosistion, zoom: 14);
+    _googleController
+        .animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
+  }
 
   @override
   Widget build(BuildContext context) {
     return GoogleMap(
+      padding: EdgeInsets.only(bottom: bottomPaddingMap),
       mapType: MapType.normal,
       myLocationButtonEnabled: true,
+      myLocationEnabled: true,
+      zoomGesturesEnabled: true,
+      zoomControlsEnabled: true,
       initialCameraPosition: CameraPosition(
         target: LatLng(37.43296265331129, -122.08832357078792),
         zoom: 14.4746,
@@ -26,6 +42,10 @@ class _MapAppState extends State<MapApp> {
       onMapCreated: (GoogleMapController controller) {
         _mapController.complete(controller);
         _googleController = controller;
+        locatePosition();
+        setState(() {
+          bottomPaddingMap = 300.0;
+        });
       },
     );
   }
