09ab896fba19708d1b43d935d8a76053c67ff2bb
toubal77 <72252709+toubal77@users.noreply.github.com>
2021-08-05 11:38:57 +0200

    get place details from drop off search

--- lib/provider/app_data.dart
+++ lib/provider/app_data.dart
@@ -2,9 +2,14 @@ import 'package:flutter/widgets.dart';
 import 'package:rider_app/models/address.dart';
 
 class AppData extends ChangeNotifier {
-  Address? pickUpLocation;
+  Address? pickUpLocation, dropOffLocation;
   void updatePickUpLocationAddress(Address pickUpAddress) {
     pickUpLocation = pickUpAddress;
+    notifyListeners();
+  }
+
+  void updateDropOffLocationAddress(Address dropOffAdress) {
+    dropOffLocation = dropOffAdress;
     notifyListeners();
   }
 }
--- lib/widgets/place_prediction_tile.dart
+++ lib/widgets/place_prediction_tile.dart
@@ -1,59 +1,97 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:rider_app/configMaps.dart';
+import 'package:rider_app/models/address.dart';
+import 'package:rider_app/provider/app_data.dart';
+import 'package:rider_app/services/apis.dart';
 
 class PredictionTile extends StatelessWidget {
   // final PlacePruedictions placePredictions;
   // PredictionTile(this.placePredictions);
+
+  void getPlaceAddressDetails(String placeId, context) async {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) => CircularProgressIndicator(),
+    );
+    String placeDetailsUrl =
+        'https://maps.googleapis.com/maps/api/place/details/json?place_id=$placeId&key=$mapKey';
+    var response = await ApiMethode.getRequest(placeDetailsUrl);
+    Navigator.pop(context);
+    if (response == 'Fieled') {
+      return;
+    }
+    if (response["status"] == "OK") {
+      Address address = Address();
+      address.placeName = response["result"]["name"];
+      address.placeId = placeId;
+      address.latitude = response["result"]["geometry"]["location"]["lat"];
+      address.longitude = response["result"]["geometry"]["location"]["lng"];
+      Provider.of<AppData>(context, listen: false)
+          .updateDropOffLocationAddress(address);
+      print('This is Drop off Location:');
+      print(address.placeName);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: [
-          SizedBox(
-            width: 4,
-          ),
-          Row(
-            children: [
-              Icon(Icons.add_location),
-              SizedBox(
-                width: 14,
-              ),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    SizedBox(
-                      height: 4,
-                    ),
-                    Text(
-                      //       placePredictions.mainText!,
-                      'main Text',
-                      overflow: TextOverflow.ellipsis,
-                      style: TextStyle(fontSize: 16),
-                    ),
-                    SizedBox(
-                      height: 2,
-                    ),
-                    Text(
-                      //  placePredictions.secondaryText!,
-                      'secondary text',
-                      overflow: TextOverflow.ellipsis,
-                      style: TextStyle(
-                        fontSize: 12,
-                        color: Colors.grey,
-                      ),
-                    ),
-                    SizedBox(
-                      height: 4,
-                    ),
-                  ],
+    return GestureDetector(
+      onTap: () {
+        //    getPlaceAddressDetails(placePredictions.placeId, context);
+      },
+      child: Container(
+        child: Column(
+          children: [
+            SizedBox(
+              width: 4,
+            ),
+            Row(
+              children: [
+                Icon(Icons.add_location),
+                SizedBox(
+                  width: 14,
                 ),
-              ),
-            ],
-          ),
-          SizedBox(
-            width: 10,
-          ),
-        ],
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        height: 4,
+                      ),
+                      Text(
+                        //       placePredictions.mainText!,
+                        'main Text',
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      SizedBox(
+                        height: 2,
+                      ),
+                      Text(
+                        //  placePredictions.secondaryText!,
+                        'secondary text',
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                          fontSize: 12,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 4,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              width: 10,
+            ),
+          ],
+        ),
       ),
     );
   }
